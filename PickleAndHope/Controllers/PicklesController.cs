6be77143a2d04ba1ff87a23ab61b6855cdba665f
Ashley Claiborne <languageisaliquid@gmail.com>
2020-04-04 13:20:12 -0500

    Add project files.

---
+++ PickleAndHope/Controllers/PicklesController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PickleAndHope.DataAccessLayer;
+using PickleAndHope.Models;
+
+namespace PickleAndHope.Controllers
+{
+    [Route("api/pickles")]
+    [ApiController]
+    public class PicklesController : ControllerBase
+    {
+        PickleRepo _repository = new PickleRepo();
+
+        [HttpPost("add")]
+        public IActionResult AddPickle(Pickle pickleToAdd)
+        {
+            var existingPickle = _repository.GetByType(pickleToAdd.Type);
+            if (existingPickle == null)
+            {
+                _repository.Add(pickleToAdd);
+                return Created("", pickleToAdd);
+            }
+            else
+            {
+                var updatedPickle = _repository.Update(pickleToAdd);
+                return Ok(updatedPickle);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult GetAllPickles()
+        {
+            var allPickles =  _repository.GetAll();
+            return Ok(allPickles);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetPickleById(int id)
+        {
+            var selectedPickle = _repository.GetById(id);
+            if (selectedPickle == null)
+            {
+                return NotFound("No pickle matching that id was found.");
+            }
+            else
+            {
+                return Ok(selectedPickle);
+            }
+        }
+    }
+}
---
+++ PickleAndHope/DataAccessLayer/PickleRepo.cs
@@ -0,0 +1,56 @@
+﻿using PickleAndHope.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PickleAndHope.DataAccessLayer
+{
+    public class PickleRepo
+    {
+        static List<Pickle> _pickles = new List<Pickle>() 
+        { 
+            new Pickle() 
+            { 
+                Id = 1, 
+                Type = "Dill", 
+                NumberInStock = 9, 
+                Price = 1.25m, 
+                Size = "large" 
+            } 
+        };
+
+        public void Add(Pickle pickle)
+        {
+            pickle.Id = _pickles.Max(p => p.Id) + 1;
+            _pickles.Add(pickle);
+        }
+
+        public void Remove(string type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Pickle Update(Pickle pickle)
+        {
+            var pickleToUpdate = GetByType(pickle.Type);
+            pickleToUpdate.NumberInStock += pickle.NumberInStock;
+            return pickleToUpdate;
+        }
+
+        public Pickle GetByType(string type)
+        {
+            return _pickles.FirstOrDefault(p => p.Type == type);
+        }
+
+        public List<Pickle> GetAll()
+        {
+            return _pickles;
+        }
+
+        public Pickle GetById(int id)
+        {
+            return _pickles.FirstOrDefault(p => p.Id == id);
+        }
+    }
+}
